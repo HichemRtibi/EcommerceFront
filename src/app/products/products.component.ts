666c0a643b24633a7bc183eead8da28ecfeaf920
hichem rtibi <hichem.rtibi@gmail.com>
2019-08-13 15:13:02 +0200

    afficher les products de server

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {CatalogueService} from '../services/catalogue.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  private prodcuts;
+
+  constructor(private catalogueservices:CatalogueService) { }
+
+  ngOnInit() {
+  }
+  getProducts()
+  {
+    this.catalogueservices.getResources("/products/search/selectedProducts").subscribe(data=>{
+      this.prodcuts=data;
+      },error1 => {
+      console.log(error1)
+      }
+    )
+  }
+
+}
