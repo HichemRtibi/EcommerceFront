6d64f4de0fbc0ac2bb053d32de5ef2e52429125e
hichem rtibi <hichem.rtibi@gmail.com>
2019-08-13 02:32:27 +0200

    creation service and configure url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogueService} from './services/catalogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  categories;
+
+  constructor(private catservice: CatalogueService) {
+
+  }
+
+  ngOnInit() {
+  }
+
   title = 'EcommerceFront';
+
+  private getCategories() {
+    this.catservice.getResources('/categories').subscribe(data => {
+      this.categories = data
+    }, error1 => {
+      console.log(error1)
+    })
+  }
 }
